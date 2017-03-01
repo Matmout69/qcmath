d39f86edf3c973c221856f25ad3db6ef1bc1c983
mannk <kostia.mannevy@gmail.com>
2017-03-01 13:36:34 +0100

    Essais de routage

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?php echo phpversion() ?>
+/* Page principale du site, appelée à chaque requête
+   Effectue le routage
+   Auteur : Kostia
+*/
+<?php 
+
+require '/vendor/autoload.php'; // Chargement des bibl. (Composer)
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/test', 'fonction_test');
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        break;
+}
